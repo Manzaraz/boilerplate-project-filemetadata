89e03028909994f02494508266e382545f3bd3d0
Manzaraz <magnusmanz@gmail.com>
2022-02-01 21:57:18 -0300

    final

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express"),
+  cors = require("cors"),
+  multer = require("multer"),
+  upload = multer({ dest: "uploads/" }),
+  app = express();
 
-var app = express();
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  function (req, res, next) {
+    // console.log(req.file)
+    let { originalname, mimetype, size } = req.file;
+    console.log({ originalname, mimetype, size });
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size,
+    });
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
